cad0ff0f09cf4a5dadfd8a5e16fa0195d2f81855
sx999 <2146105925@qq.com>
2021-10-22 16:23:24 +0800

    1

--- offvue/src/views/index/Index.vue
+++ offvue/src/views/index/Index.vue
@@ -1,19 +1,21 @@
 <template>
     <el-container>
-        <el-header class="header">
-            <div class="logo-left-img">
-            <!-- image -->
-            </div>
-            <ul class="nav">
+        <el-header id="header">
+           <div class="cheader">
+                <div class="logo-left-img">
+                
+                </div>
+                <ul class="nav">
                 <li><span>首页</span></li>
                 <li><span>公司概况</span></li>
                 <li><span>新闻动态</span></li>
-                <li><span>热门赛事</span></li>
+                <li><span>热门赛事 </span></li>
                 <li><span>研学活动</span></li>
                 <li><span>专家顾问</span></li>
                 <li><span>文创商城</span></li>
                 <li><span>关于我们</span></li>
-            </ul>
+                </ul>
+            </div>  
         </el-header>
         <el-main class="content">
            <router-view/>
@@ -36,33 +38,40 @@ export default({
 </script>
 
 <style scoped>
-    .header{
+    #header{
+        /* width: 100%; */
+        max-width:1920px;
+        height: 88px !important;
+        padding: 0 448px!important;
+        background-color: #FFFFFF;
+    }
+   #header .cheader{
         display: flex;
         align-items: center;
+        justify-content: space-between;
         width: 100%;
-        height: 88px !important;
-        padding: 0 !important;
-        /* background-color: #FFFFFF; */
+        height: 100%;
+        /* background-color: rgb(54, 75, 75) */
     }
-    .header .logo-left-img{
-        margin-left: 448px;
+    #header  .cheader .logo-left-img{
+        /* margin-left: 448px; */
         width: 175px;
         height: 72px;
         background-color: aqua;
     }
-    .header .nav{
+    #header .cheader .nav{
         display: flex;
         align-items: center;
-        margin-left: 192px;
-        margin-right: 454px;
+    
         width:650px;
     }
-    .header .nav li{
-        width: 12.5%;
+    #header .cheader .nav li{
+        width: 81.25px;
+        /* width: 12.5%; */
         font-size: 16px;
         text-align: center;
     }
-    .header .nav li span{
+    #header .cheader .nav li span{
         border-bottom: 3px solid #F2C723;
     }
     .content{
