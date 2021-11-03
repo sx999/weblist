8873ae14b7f5452d916161b0276451a5d952ad25
sx999 <2146105925@qq.com>
2021-11-03 15:47:16 +0800

    11.3下午

--- offvue/src/views/index/ExpertAdvisor.vue
+++ offvue/src/views/index/ExpertAdvisor.vue
@@ -1,6 +1,40 @@
 <template>
     <div>
-        <h1>专家顾问</h1>
+         <!-- 头部轮播 -->
+        <div class="ExpertAdvisort-box">
+            <img src="../../assets/images/img3.jpg" alt="">
+            <div class="posit">   
+                <p class="posit-p1">专家顾问</p>
+                <p class="posit-p2">专业教育学家提供服务支持</p>
+            </div>
+            
+        </div>
+        <!-- 内容 -->
+        <div class="ExpertAdvisor-content">
+            <div class="title">
+                <p>专家顾问</p>
+            </div>
+            <div class="ExpertAdvisor-list">
+                <div class="list-l">
+                    <p class="p1">姓名</p>
+                    <p class="p2">中国著名教育学家、书法家、诗人；中国著名教育学家、书 法家、诗人；中国著名教育学家、书法家、诗人；中国著名 教育学家、书法家、诗人；</p>
+                    <div class="botm-h1"></div>
+                </div>
+                <div class="list-r mar-l-16">
+                    <img src="../../assets/images/19.jpg" alt="">
+                </div>
+            </div>
+            <div class="ExpertAdvisor-list">
+                <div class="list-r mar-l16">
+                    <img src="../../assets/images/19.jpg" alt="">
+                </div>
+                <div class="list-l mar-l-16">
+                    <p class="p1">姓名</p>
+                    <p class="p2">中国著名教育学家、书法家、诗人；中国著名教育学家、书 法家、诗人；中国著名教育学家、书法家、诗人；中国著名 教育学家、书法家、诗人；</p>
+                    <div class="botm-h1"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -13,5 +47,5 @@ export default {
 }
 </script>
 <style scoped>
-
+     @import '../../assets/css/ExpertAdvisorStyle.css';
 </style>
